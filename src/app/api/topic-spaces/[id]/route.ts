7402cee363aaf8a29705ccaa6a451e59d3ec9d39
松野仁志 <MatsunoHitoshi1109Kumamoto@gmail.com>
2025-03-25 18:30:28 +0900

    cutOff値を元に戻す

--- src/app/api/topic-spaces/[id]/route.ts
+++ src/app/api/topic-spaces/[id]/route.ts
@@ -8,7 +8,7 @@ export const GET = async (
   try {
     const res = await api.topicSpaces.getByIdPublic({
       id: params.id,
-      filterOption: { type: "tag", value: "main", cutOff: "1" },
+      filterOption: { type: "tag", value: "main", cutOff: "2" },
     });
     return NextResponse.json({
       id: res.id,
